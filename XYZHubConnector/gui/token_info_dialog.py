852b17d41103b649d011037c030959c547224cc4
minff <16268924+minff@users.noreply.github.com>
2020-04-26 18:09:35 +0200

    draft: refactor token info dialog, add server info dialog

--- XYZHubConnector/gui/token_info_dialog.py
+++ XYZHubConnector/gui/token_info_dialog.py
@@ -13,18 +13,30 @@ from . import get_ui_class
 
 TokenEditUI = get_ui_class('edit_token_dialog.ui')
 
-class TokenInfoDialog(QDialog, TokenEditUI):
+class NameValueDialog(QDialog, TokenEditUI):
     title = "XYZ"
+    txt_value="Value"
     def __init__(self, parent=None):
         """init window"""
         QDialog.__init__(self, parent)
         TokenEditUI.setupUi(self,self)
         self.setWindowTitle(self.title)
+        self.label_value.setText(self.txt_value)
         
         self.lineEdit_name.textChanged.connect(self.ui_enable_btn)
         self.lineEdit_token.textChanged.connect(self.ui_enable_btn)
         self.ui_enable_btn()
         
+    def ui_enable_btn(self):
+        flag = all([
+            self.lineEdit_name.text().strip(),
+            self.lineEdit_token.text().strip()
+            ])
+        self.buttonBox.button(self.buttonBox.Ok).setEnabled(flag)
+        self.buttonBox.button(self.buttonBox.Ok).clearFocus()
+
+class TokenInfoDialog(NameValueDialog):
+    txt_value="Token"
     def get_token_info(self):
         d = {
             "name": self.lineEdit_name.text(),
@@ -35,15 +47,26 @@ class TokenInfoDialog(QDialog, TokenEditUI):
         self.lineEdit_name.setText(token_info.get("name",""))
         self.lineEdit_token.setText(token_info.get("token",""))
         
-    def ui_enable_btn(self):
-        flag = all([
-            self.lineEdit_name.text().strip(),
-            self.lineEdit_token.text().strip()
-            ])
-        self.buttonBox.button(self.buttonBox.Ok).setEnabled(flag)
-        self.buttonBox.button(self.buttonBox.Ok).clearFocus()
-
 class NewTokenInfoDialog(TokenInfoDialog):
     title = "Add new token"
 class EditTokenInfoDialog(TokenInfoDialog):
     title = "Edit token"
+
+
+class ServerInfoDialog(NameValueDialog):
+    txt_value="Server"
+    def get_server_info(self):
+        d = {
+            "name": self.lineEdit_name.text(),
+            "server": self.lineEdit_token.text(),
+        }
+        return d
+    def set_server_info(self, token_info):
+        self.lineEdit_name.setText(token_info.get("name",""))
+        self.lineEdit_token.setText(token_info.get("server",""))
+        
+class NewServerInfoDialog(TokenInfoDialog):
+    title = "Add new server"
+    
+class EditServerInfoDialog(TokenInfoDialog):
+    title = "Edit server"
